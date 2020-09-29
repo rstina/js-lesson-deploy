39b8da5e7e35d6d4db26f687616904fdd9ca6ebf
rstina <53047495+rstina@users.noreply.github.com>
2020-09-29 09:54:43 +0200

    Updates

--- precache-manifest.4f8c42fade80ec31c557dcc163a04ae5.js
+++ precache-manifest.4f8c42fade80ec31c557dcc163a04ae5.js
@@ -1,10 +1,10 @@
 self.__precacheManifest = (self.__precacheManifest || []).concat([
   {
-    "revision": "d8028f87c3bf2e8f0e1a68642bfb062f",
+    "revision": "d2f028fdbbe176e9e86c4f6e10cd1797",
     "url": "/js-lesson-deploy/index.html"
   },
   {
-    "revision": "5be8e7a0524937680328",
+    "revision": "a9cfc636bef33a23b7e7",
     "url": "/js-lesson-deploy/static/css/main.5ecd60fb.chunk.css"
   },
   {
@@ -16,8 +16,8 @@ self.__precacheManifest = (self.__precacheManifest || []).concat([
     "url": "/js-lesson-deploy/static/js/2.af32eef5.chunk.js.LICENSE.txt"
   },
   {
-    "revision": "5be8e7a0524937680328",
-    "url": "/js-lesson-deploy/static/js/main.62773783.chunk.js"
+    "revision": "a9cfc636bef33a23b7e7",
+    "url": "/js-lesson-deploy/static/js/main.d05826f9.chunk.js"
   },
   {
     "revision": "328f0ec1c4f51601524f",
